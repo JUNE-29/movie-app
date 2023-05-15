55490accd10432e18936f3663e7ab4ffb4694e16
JUNE-29 <jiyuni529@gmail.com>
2023-05-16 03:13:20 +0900

    [add] movie Trending List 가져오는 기능 추가

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
+    const [movies, setMovies] = useState();
+
+    useEffect(() => {
+        movieResult().then((movie) => setMovies(movie));
+    }, []);
+
     return (
         <>
-            <h1>Movies</h1>
+            <h1>Trending</h1>
+            {movies && (
+                <ul>
+                    {movies.map((movie) => (
+                        <li key={movie.id}>{movie.title}</li>
+                    ))}
+                </ul>
+            )}
         </>
     );
+}
+
+async function movieResult() {
+    const client = axios.create({
+        baseURL: "https://api.themoviedb.org/3/",
+        params: { api_key: process.env.REACT_APP_TMDB_API_KEY },
+    });
+
+    const res = await client
+        .get("trending/movie/week", {
+            params: {
+                language: "ko-KR",
+                adult: false,
+            },
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    return res.data.results;
 }
