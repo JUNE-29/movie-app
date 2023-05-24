849aa87d4a21c39812e72002678e0806780baefc
JUNE-29 <jiyuni529@gmail.com>
2023-05-24 16:41:36 +0900

    [add] 네비게이션바 추가, 스타일링

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { RiSearchLine } from "react-icons/ri";
+
+export default function Header() {
+    return (
+        <header className="flex justify-between">
+            <section className="flex-1">
+                <img src="/images/logo.png" alt="logo" />
+            </section>
+            <section className="flex-1 relative">
+                <button className="absolute right-0">
+                    <RiSearchLine />
+                </button>
+                <input
+                    className="w-full bg-backgroundColor border-0 border-b"
+                    placeholder="영화를 검색해보세요!"
+                />
+                <div className="flex flex-col gap-2 w-1/2">
+                    <p className="pb-1 mt-2 border-b cursor-pointer hover:text-gray-300">
+                        트렌딩
+                    </p>
+                    <p className="pb-1 border-b cursor-pointer hover:text-gray-300">
+                        인기 있는 영화
+                    </p>
+                    <p className="pb-1 border-b cursor-pointer hover:text-gray-300">
+                        현재 상영중인 영화
+                    </p>
+                    <p className="pb-1 border-b cursor-pointer hover:text-gray-300">
+                        개봉예정인 영화
+                    </p>
+                </div>
+            </section>
+        </header>
+    );
+}
