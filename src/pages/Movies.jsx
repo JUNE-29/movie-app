b542fe936c7afe2da6648ddaaef88f7fc8800244
JUNE-29 <jiyuni529@gmail.com>
2023-05-24 16:40:40 +0900

    [add]popular, nowPlaying, upcoming list 받아오는 기능 추가

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,19 +2,58 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Movies() {
-    const [movies, setMovies] = useState();
+    const [trendingMovies, setTrendingMovies] = useState();
+    const [popularMovies, setPopularMovies] = useState();
+    const [nowPlaingMovies, setNowPlayingMovies] = useState();
+    const [upcomingMovies, setUpcomingMovies] = useState();
 
     useEffect(() => {
-        movieResult().then((movie) => setMovies(movie));
+        getTrendingMovies().then((movie) => setTrendingMovies(movie));
+        getPopularMovies().then((movie) => setPopularMovies(movie));
+        getNowPlayingMovies().then((movie) => setNowPlayingMovies(movie));
+        getUpcomingMovies().then((movie) => setUpcomingMovies(movie));
     }, []);
 
     return (
         <>
             <h1>Trending</h1>
-            {movies && (
+            {trendingMovies && (
                 <ul>
-                    {movies.map((movie) => (
+                    {trendingMovies.map((movie) => (
                         <li key={movie.id}>{movie.title}</li>
+                    ))}
+                </ul>
+            )}
+            <hr></hr>
+            <h1>Popular</h1>
+            {popularMovies && (
+                <ul>
+                    {popularMovies.map((movie) => (
+                        <li key={movie.id}>
+                            {movie.title} {movie.vote_average}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <hr></hr>
+            <h1>Now Playing</h1>
+            {nowPlaingMovies && (
+                <ul>
+                    {nowPlaingMovies.map((movie) => (
+                        <li key={movie.id}>
+                            {movie.title} {movie.vote_average}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <hr></hr>
+            <h1>Upcoming</h1>
+            {upcomingMovies && (
+                <ul>
+                    {upcomingMovies.map((movie) => (
+                        <li key={movie.id}>
+                            {movie.title} {movie.vote_average}
+                        </li>
                     ))}
                 </ul>
             )}
@@ -22,17 +61,65 @@ export default function Movies() {
     );
 }
 
-async function movieResult() {
-    const client = axios.create({
-        baseURL: "https://api.themoviedb.org/3/",
-        params: { api_key: process.env.REACT_APP_TMDB_API_KEY },
-    });
+const client = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    params: { api_key: process.env.REACT_APP_TMDB_API_KEY },
+});
 
+async function getTrendingMovies() {
     const res = await client
         .get("trending/movie/week", {
             params: {
                 language: "ko-KR",
                 adult: false,
+            },
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    return res.data.results;
+}
+
+async function getPopularMovies() {
+    const res = await client
+        .get("movie/popular", {
+            params: {
+                language: "ko-KR",
+                adult: false,
+                page: 1,
+            },
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    return res.data.results;
+}
+
+async function getNowPlayingMovies() {
+    const res = await client
+        .get("movie/now_playing", {
+            params: {
+                language: "ko-KR",
+                adult: false,
+                page: 1,
+                sort_by: "popularity.desc",
+                region: "KR",
+            },
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    return res.data.results;
+}
+
+async function getUpcomingMovies() {
+    const res = await client
+        .get("movie/upcoming", {
+            params: {
+                language: "ko-KR",
+                adult: false,
+                page: 1,
+                region: "KR",
             },
         })
         .catch((error) => {
