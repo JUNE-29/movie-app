6bf8f31072449e0c66aa36345269a6080ddbe69b
JUNE-29 <jiyuni529@gmail.com>
2023-05-28 21:22:25 +0900

    [mod] NotFound 페이지에 Home으로 돌아가는 링크 추가

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
+    const navigate = useNavigate();
     return (
         <div className="text-center m-5">
             <p>페이지를 찾을 수 없습니다.</p>
-            <button>메인으로 돌아가기</button>
+            <button onClick={() => navigate("/")}>메인으로 돌아가기</button>
         </div>
     );
 }
